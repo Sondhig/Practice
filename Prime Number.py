421e407d95eacc3a72b15c6731f00cdbf3254897
Sondhig <94097282+Sondhig@users.noreply.github.com>
2023-09-14 22:09:21 -0400

    Update Prime Number.py

--- Prime Number.py
+++ Prime Number.py
@@ -1,21 +1,4 @@
-# This is a Python script that prints a bunch of statements
-
-# Printing a single statement
-print("Hello, I'm a Python script!")
-
-# Printing multiple statements
-print("Here are some more statements:")
-print("Statement 1")
-print("Statement 2")
-print("Statement 3")
-
-# Printing statements with variables
-name = "Alice"
-age = 30
-print(f"My name is {name} and I am {age} years old.")
-
-
-
+print("Script 2")
 
 
 
