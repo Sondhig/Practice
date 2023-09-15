c81ed52b3e9ee600fcfd136981d9deab25bbcdc2
Sondhig <94097282+Sondhig@users.noreply.github.com>
2023-09-14 22:09:05 -0400

    Update Perfect Square.py

--- Perfect Square.py
+++ Perfect Square.py
@@ -1,9 +1,1 @@
-x=input("Enter a Number that may be a Perfect Square ")
-flag=0
-for i in range(int(int(x)/2)+1):
-    if(i*i==int(x)):
-        flag=1
-if(flag==1):
-    print("Is a Perfect Square")
-else:
-    print("Not")
+print("Script 1")
